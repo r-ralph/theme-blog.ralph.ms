a9aa49dc2bb6489ec90f3d041cefa2a2698b34de
Ralph <git@ralph.ms>
2020-07-17 21:56:16 +0900

    Replace lightbox library

--- assets/js/even.js
+++ assets/js/even.js
@@ -157,21 +157,6 @@ Even._initToc = function() {
   });
 };
 
-Even.fancybox = function() {
-  if ($.fancybox) {
-    $('.post-content').each(function() {
-      $(this).find('img').each(function() {
-        $(this).wrap(`<a class="fancybox" href="${this.src}" data-fancybox="gallery" data-caption="${this.title}"></a>`);
-      });
-    });
-
-    $('.fancybox').fancybox({
-      selector: '.fancybox',
-      protect: true,
-    });
-  }
-};
-
 Even.chroma = function() {
   const blocks = document.querySelectorAll('.highlight > .chroma');
   for (let i = 0; i < blocks.length; i++) {
